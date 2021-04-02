6af2e5af828adfb1c60caa4ae5ec85245fd69dbf
unknown <jadisnou@gmail.com>
2021-04-02 17:19:38 +0200

    working day

---
+++ exercice34/js/main.js
@@ -0,0 +1,22 @@
+
+$("button").click(function () {
+
+
+    
+    $.ajax(
+        
+        {
+            
+            url:  'https://restcountries.eu/rest/v2/name/' + $("input").val() ,
+
+            success: function (data, statuts, response) {
+
+                $("#country").html(data[0].name);
+                $("#capital").html(data[0].capital);
+
+            }
+        });
+
+
+
+});
